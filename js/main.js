3e0be801bd3cf144bd49339003e04577e1b85978
AsianDesigner <JustinRianReyes2@gmail.com>
2024-02-29 08:58:53 -0500

    bug fix 2

--- js/main.js
+++ js/main.js
@@ -6,7 +6,7 @@
 // let theButton = document.querySelector("#buttonOne");
 
 // create a 1 to many connection with a variable -> querySelectorAll("queryString")
-let theButtons = document.querySelectorAll("#buttonHolder img"),
+let theButtons = document.querySelectorAll("#buttonHolder img", "#resetBut button"),
 	theHeading = document.querySelector("#headLine h1"),
 	puzzleBoard = document.querySelector(".puzzle-board"),
 	puzzlePieces = document.querySelectorAll(".puzzle-pieces img"),
@@ -22,10 +22,18 @@ function changeBGImage() {
 	// the `` is a JavaScript template string. It tells the JS enging to evaluate the expression
 	// inside the braces - run that little bit of code. In this case it's just pulling the ID of the
 	// button we clicked on and putting it at the end of the image name (0, 1, 2, 3)
-	// and updating the background-image style of the puzzle board element.
+	// and updating the background-image style of the puzzle board element
 
 	// bug fix #2 should go here. it's at most 3 lines of JS code.
+	if (document.querySelector(".puzzle-board").children.length > 0) {
+		console.log('new board selected!')
+		puzzlePieces.forEach(piece => document.querySelector(".puzzle-pieces").appendChild(piece));
+	}
 	puzzleBoard.style.backgroundImage = `url(images/backGround${this.id}.jpg)`;
+}
+
+function resetPuzzle() {
+	puzzlePieces.forEach(piece => document.querySelector(".puzzle-pieces").appendChild(piece));
 }
 
 function handleStartDrag() { 
@@ -46,7 +54,7 @@ function handleDrop(e) {
 	console.log('dropped something on me');
 	// bug fix #1 should go here, and it's at most 3 lines of JS code
 	if (this.children.length > 0) {
-        console.log('Drop zone already contains a piece. Choose another spot.');
+        console.log('this space is occupied!');
         return;
     }
 	// this line is going to move the dragged piece from the left side of the board
@@ -63,6 +71,8 @@ function handleDrop(e) {
 // 1 to many event handling
 // add event handling to each button in the collection of buttons, one at a time
 theButtons.forEach(button => button.addEventListener("click", changeBGImage));
+
+resetBut.addEventListener("click", resetPuzzle);
 
 // add the drag event handling to the puzzle pieces
 puzzlePieces.forEach(piece => piece.addEventListener("dragstart", handleStartDrag));
